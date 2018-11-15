61f459f8f7f24f3adc80b2c0f7f38a2aac101df6
Michael Aaron Murphy <mmstickman@gmail.com>
2018-11-15 16:38:11 -0700

    Add more architectures

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
 use std::{fs, io, str::FromStr};
 
-#[derive(Debug, Clone, Hash, PartialEq)]
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq, Ord, PartialOrd)]
 pub enum Architecture {
+    Any,
     All,
     Amd64,
-    Arm64,
+    Armel,
     Armhf,
+    Arm64,
     I386,
-    Ppc64el,
-    S390x,
+    Mips,
+    Mipsel,
+    Mips64,
+    Mips64El,
+    Ppc64El,
+    S390X,
+    LinuxAny,
+    X32,
 }
 
 impl FromStr for Architecture {
@@ -16,13 +24,21 @@ impl FromStr for Architecture {
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         let arch = match input {
+            "any" => Architecture::Any,
             "all" => Architecture::All,
             "amd64" => Architecture::Amd64,
-            "arm64" => Architecture::Arm64,
+            "armel" => Architecture::Armel,
             "armhf" => Architecture::Armhf,
+            "arm64" => Architecture::Arm64,
             "i386" => Architecture::I386,
-            "ppc64el" => Architecture::Ppc64el,
-            "s390x" => Architecture::S390x,
+            "mips" => Architecture::Mips,
+            "mipsel" => Architecture::Mipsel,
+            "mips64" => Architecture::Mips64,
+            "mips64el" => Architecture::Mips64El,
+            "ppc64el" => Architecture::Ppc64El,
+            "s390x" => Architecture::S390X,
+            "linux-any" => Architecture::LinuxAny,
+            "x32" => Architecture::X32,
             _ => return Err("invalid architecture")
         };
 
@@ -33,13 +49,21 @@ impl FromStr for Architecture {
 impl From<Architecture> for &'static str {
     fn from(arch: Architecture) -> Self {
         match arch {
+            Architecture::Any => "any",
             Architecture::All => "all",
             Architecture::Amd64 => "amd64",
-            Architecture::Arm64 => "arm64",
+            Architecture::Armel => "armel",
             Architecture::Armhf => "armhf",
+            Architecture::Arm64 => "arm64",
             Architecture::I386 => "i386",
-            Architecture::Ppc64el => "ppc64el",
-            Architecture::S390x => "s390x",
+            Architecture::Mips => "mips",
+            Architecture::Mipsel => "mipsel",
+            Architecture::Mips64 => "mips64",
+            Architecture::Mips64El => "mips64el",
+            Architecture::Ppc64El => "ppc64el",
+            Architecture::S390X => "s390x",
+            Architecture::LinuxAny => "linux-any",
+            Architecture::X32 => "x32",
         }
     }
 }
